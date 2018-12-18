43f7114958836df60c5c29c1d402cc7939b80ad1
Amir Ismagilov <progamailism@gmail.com>
2018-12-18 19:07:59 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+   <meta charset="utf-8"/>
+</head>
+
+<body>
+    <?php 
+        echo "<p>Agile</br></p>";
+        echo "<p>Hello shop!</br></p>"
+     ?>
+</body>
+
+</html>
