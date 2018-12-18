297ca55782e1126dfd9d9317ff162716a5dda27b
Amir Ismagilov <progamailism@gmail.com>
2018-12-18 19:18:05 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 
 <body>
     <?php 
-        echo "<p>Agile</br></p>";
-        echo "<p>Hello shop!</br></p>"
+        echo "<h1>Hello Shop!</h1>";
+        echo "<p>I'm Ismagilov Amir Zakirovich Student USATU using Heroku with Github!</p>";
+        echo "<h3>Contacts</h3>";
+        echo "<p>email: progamailism@yandex.ru</p>";
+        echo "<p>Facebook: @ismagilovamir</p>";
+        echo "<p>Twitter: @AmirIsmagilov</p>";
+        echo "<p>VK | Vkontakte: @amirismagilov</p>";
      ?>
 </body>
 
